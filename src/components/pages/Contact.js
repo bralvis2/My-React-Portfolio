3191b3a93a6e3611a640c7e680d2dfd672824e75
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2022-09-14 22:40:03 -0500

    added contact info and form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,15 +2,40 @@ import React from 'react';
 // style sheet
 import './Contact.css'
 
+
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Me</h1>
-      <ul>
-        <li> Email </li>
-        <li> Phone Number </li>
-        <li> Linkedin </li>
-      </ul>
+    <div className='contactContainer'>
+      <div className='contactInfo'>
+        <h1>Contact Me</h1>
+        <ul className='contactList'>
+          <li>📧: bralvis2@gmail.com </li>
+          <li>📱: (123)456-7890 </li>
+          <li>
+            <a href='https://github.com/bralvis2' target="_blank" className='github'> 👩🏽‍💻: Github</a>
+          </li>
+        </ul>
+        <p className='contactMe'>
+          If you are intested in collaborating or if you have any questions, please fill out this form. 
+        </p>
+      </div>
+      <div className='contactForm'>
+        <form>
+          <div class="form-group">
+            <label for="formGroupExampleInput">Name</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="👤" />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="📧" />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Message</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <button type="submit" class="btn">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
