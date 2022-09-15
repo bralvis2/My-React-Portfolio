680d797af8f5a0beb76deb9ae56c6a1142edad47
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2022-09-14 21:47:25 -0500

    Active links to icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import git from '../images/git.png'
+import linked from '../images/linkedin.png'
+import twitter from '../images/twitter.png'
 // style sheet
 import '../styles/Footer.css'
 
-export default function Footer(){
+export default function Footer() {
     return (
-        <footer> insert footer here </footer>
+        <footer>
+            <a href='https://github.com/bralvis2' target="_blank">
+                <img src={git} alt='github icon and link' width={'50px'} className='icons' />
+            </a>
+            <a href='https://www.linkedin.com/in/brenda-alvis-11639a241/'target="_blank">
+                <img src={linked} alt='linkedin icon and link' width={'85px'} className='icons' />
+            </a>
+            <a href='https://twitter.com/bralvis2'target="_blank">
+                <img src={twitter} alt='twitter icon and link' width={'50px'} className='icons' />
+            </a>
+        </footer>
     )
 }
